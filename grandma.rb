3cebb14f7f9cb6ccc090cb7c3ef1cf61e099a4d3
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-13 13:07:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
-  if "" == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif "".capitalise
+  elsif phrase.capitalise
     puts "NO, NOT SINCE 1989!"
   else
     puts "HUH?! SPEAK UP SUNNY!"
